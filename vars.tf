d929857740d044b84268b5736fce8f1255c93afd
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-09-15 12:00:27 +0530

    .....

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "bastion_cidr" {}
+# variable "bastion_cidr" {}
 variable "component" {
   default = "rabbitmq"
 }
